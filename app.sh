e03266b44f100df40d2a2f35500d6935d3883e15
na <81983672+yuanjv@users.noreply.github.com>
2024-11-02 21:52:06 -0700

    Update app.sh

--- app.sh
+++ app.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 addr="user@ip"
 app=""
 
@@ -21,13 +23,12 @@ tmux send-keys -t $app:0.1 "DISPLAY=:0 ssh -Y $addr" ENTER
 
 #app launch commands
 
-#if it is flatpak app
+#if it is a flatpak app
 flatpak_id=''
 tmux send-keys -t $app:0.1 "flatpak run --socket=x11 --share=network --device=dri $flatpak_id" ENTER
 
 
 #make sure x window always max
-
 tmux split-window -v -t $app:0.0
 tmux send-keys -t $app:0.1 'export DISPLAY=:0' ENTER
 tmux send-keys -t $app:0.1 "while true; do xdotool search $app | xargs -I {} xdotool windowsize {} 100% 100% && xdotool search $app | xargs -I {} xdotool windowmove {} 0 0; sleep 1; done 2>/dev/null" ENTER
